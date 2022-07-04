297d6bf98fab8199da5b5cb451a6367950d476f0
John Maclean <tribute2johnny@gmail.com>
2022-07-04 23:26:06 +0100

    First Commit

---
+++ precourse_recap.py
@@ -0,0 +1,28 @@
+num_01 = 6.82
+num_02 = 3.14
+num_03 = 8.55
+
+num_total = num_01 + num_02 + num_03
+
+print(num_total)
+
+print(round(num_total))
+
+
+animal_01 = "sheep"
+animal_02 = "pig"
+animal_03 = "cow"
+animal_04 = "dog"
+
+list_of_animals = animal_01, animal_02, animal_03, animal_04
+
+print(list_of_animals)
+print(animal_01.capitalize())
+print(animal_02.swapcase())
+print(animal_03.upper())
+print(animal_04.replace("dog","cat"))
+
+
+
+
+
